543e3a6871c4d41ec97603c6c9573ba47cfb66bf
Diego Islas Ocampo <diego.islas@blac-sheep.com>
2019-09-16 21:17:30 -0500

    Add endpoint authentication, dependencies check and missing endpoints

--- README.md
+++ README.md
@@ -1,2 +1,34 @@
 # Dojour WordPress Plugin
 
+This plugin creates a new post type `dojour_event` and establishes a custom API to interact with such post types, allowing the creation, edition and deletion of these posts remotely.
+
+## Requirements
+
+* PHP >= 7.2
+* WordPress >= 5.2
+* [Application Passwords Plugin](https://wordpress.org/plugins/application-passwords/)
+
+## Authentication
+By default, WordPress uses a cookie based authentication system which means that once a user is logged, it can perform any actions on the blog if that cookie is present, however, since we want to interact with the sites on the server side, we can't rely on cookies as we don't have them.
+
+Since we can't leave the endpoints open due to security reasons, this plugin requires the installation of another one to perform the authorization and authentication procedures. [Application Passwords Plugin](https://wordpress.org/plugins/application-passwords/) was chosen because of its simplicity and it being actively maintained.
+
+### Authorization Flow
+To request the creation of an application password, the flow is similar to that of OAuth:
+
+Show the user the grant screen by redirecting them to this path on their WordPress site:
+
+```
+/wp-admin/admin.php?page=auth_app
+```
+
+Add the following URL parameters to the URL to provide information of your app:
+
+| Parameter | Required | Description |
+| --------- | -------- | ----------- |
+| `app_name` | Yes | The human readable identifier for your app. This will be the name of the generated application password, so structure it like … “WordPress Mobile App on iPhone 12” for uniqueness between multiple versions. If omitted, the user will be required to provide an application name. |
+| `success_url` | No | The URL that you’d like the user to be sent to if they approve the connection. Two GET variables will be appended when they are passed back — user_login and password — these credentials can then be used for API calls. If the success_url variable is omitted, a password will be generated and displayed to the user, to manually enter into your application. |
+| `reject_url` | No | If included, the user will get sent there if they reject the connection. If omitted, the user will be sent to the success_url, with ?success=false appended to the end. If the success_url is omitted, the user will be sent to their dashboard. |
+
+
+Once the user accepts, a new password will be created and the user will be redirected to the `success_url` you provided with two parameters in the URL that need to be saved as they are required to perform API calls, `user_login` and `password`.
--- dojour.php
+++ dojour.php
@@ -13,7 +13,7 @@
  * Text Domain:       dojour
  */
 
-// Exit if accessed directly.
+// Exit if this file is beint tried to be accessed directly.
 if (!defined ('ABSPATH')) {
 	exit;
 }
@@ -31,55 +31,84 @@ final class Dojour {
 		return self::$instance;
 	}
 
+	public static function check_dependencies () {
+		if (!class_exists ('Application_Passwords')) {
+			self::show_message ('error', '<a href="https://wordpress.org/plugins/application-passwords/" target="_blank" rel="noopener noreferrer">Application Passwords Plugin</a> is required for the <a href="https://dojour.us/" target="_blank" rel="noopener noreferrer">Dojour</a> plugin to work. Please make sure it has been installed and that it is activated.');
+		}
+	}
+
+	public static function show_message ($level = '', $message = '') {
+		echo "<div class='notice notice-$level is-dismissible'><p>$message</p></div>";
+	}
+
 	public static function activate () {
 		add_option ('dojour_settings', [
 			'username' => ''
 		]);
 	}
 
-	public function deactivate () {
+	public static function deactivate () {
 		flush_rewrite_rules ();
 	}
 
-	public function uninstall () {
+	public static function uninstall () {
 		delete_option ('dojour_settings');
 		unregister_post_type ('dojour_event');
 		flush_rewrite_rules ();
 	}
 
 	public static function setup_custom_endpoints () {
-		register_rest_route(self::$api_namespace, '/event', array(
+		register_rest_route (self::$api_namespace, '/event', array(
 			'methods' => 'POST',
 			'callback' => array ('Dojour', 'create_event'),
+			'permission_callback' => array ('Dojour', 'authorize_request')
 		));
 
-		register_rest_route('dojour/v1', '/event', array(
+		register_rest_route (self::$api_namespace, '/event', array(
 			'methods' => 'PUT',
 			'callback' => array ('Dojour', 'update_event'),
+			'permission_callback' => array ('Dojour', 'authorize_request')
 		));
 
-		register_rest_route('dojour/v1', '/event', array(
+		register_rest_route (self::$api_namespace, '/event', array(
 			'methods' => 'DELETE',
 			'callback' => array ('Dojour', 'delete_event'),
+			'permission_callback' => array ('Dojour', 'authorize_request')
 		));
+	}
+
+	public static function authorize_request () {
+		// Get HTTP request headers
+		$auth = apache_request_headers();
+
+		$authorization = $auth['Authorization'];
+
+		$user = Application_Passwords::authenticate ($authorization, $_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+
+		if ($user instanceof WP_User) {
+			// get the id use return $user->ID;
+			return true;
+		} else {
+			return false;
+		}
 	}
 
 
 	public static function setup_post_type () {
-		// register the "event" custom post type
+		// Register the "dojour_event" custom post type
 		register_post_type ('dojour_event', [
 			'label' => _('Dojour Events'),
 			'labels' => [
 				'name' => _('Dojour Events'),
 				'singular_name' => _('Dojour Event')
 			],
-			'description' => 'A dopjour evet',
+			'description' => 'A dopjour event',
 			'public' => true,
 			'show_ui' => true,
 			'has_archive' => true,
 			'rewrite' => array('slug' => 'dojour-events'),
-			'supports' => array('title', 'editor', 'thumbnail')
-		] );
+			'supports' => array('title', 'thumbnail')
+		]);
 
 		flush_rewrite_rules ();
 	}
@@ -104,7 +133,30 @@ final class Dojour {
 
 		self::fetch_image_for_post ($params['photo']['file'], $id);
 
+		add_post_meta ($id, 'remote_id', $params['id'], true);
+		add_post_meta ($id, 'remote_url', $params['absolute_url'], true);
+
 		return $id;
+	}
+
+	public static function find_post ($remote_url) {
+		$posts = get_posts ([
+			'numberposts' => 1,
+			'post_type'  => 'dojour_event',
+			'meta_key' => 'remote_id',
+			'meta_value' => $remote_url
+		]);
+
+		if (count ($posts) > 0) {
+			$post = $posts[0];
+
+			if ($post instanceof WP_Post) {
+				return $post -> ID;
+			}
+
+			return $post;
+		}
+		return null;
 	}
 
 	public static function fetch_image_for_post ($url, $post_id) {
@@ -119,23 +171,44 @@ final class Dojour {
 
 	public static function update_event ($request) {
 		$params = $request -> get_json_params ();
+
+		$post_id = self::find_post ($params['id']);
+
+		if ($post_id !== null) {
+			$post = [
+				'ID' => $post_id,
+				'post_title' => $params['title'],
+				'post_content' => $params['description']
+			];
+
+			wp_update_post ($post);
+
+			update_post_meta ($post_id, 'remote_url', $params['absolute_url']);
+		}
 	}
 
 	public static function delete_event ($request) {
 		$params = $request -> get_json_params ();
+
+		$post_id = self::find_post ($params['id']);
+
+		if ($post_id !== null) {
+			wp_delete_attachment ($post_id, true);
+			wp_delete_post ($post_id, true);
+		}
 	}
 
 	private function __construct () {
         // do other stuff here
     }
-
-
 }
 
 register_activation_hook (__FILE__, array ('Dojour', 'activate'));
 register_deactivation_hook (__FILE__, array ('Dojour', 'deactivate'));
 register_uninstall_hook (__FILE__, array ('Dojour', 'uninstall'));
 
-add_action ('rest_api_init', array ('Dojour', 'setup_custom_endpoints'));
 add_action ('init', array ('Dojour', 'setup_post_type'));
+add_action ('init', array ('Dojour', 'check_dependencies'));
+add_action ('rest_api_init', array ('Dojour', 'setup_custom_endpoints'));
+
 ?>
